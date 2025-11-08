ae8f01258d7034b6b693f0246ca24e621d4e50ac
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2025-11-07 21:28:12 -0500

    feat: Creating the user class and configuring Hibernate to convert the names of tables and columns

---
+++ auth-service/src/main/java/com/mazr/iaz/microservices/entity/User.java
@@ -0,0 +1,58 @@
+package com.mazr.iaz.microservices.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String avatar;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private Boolean accountLocked = false;
+    
+    private Integer failedLoginAttempts;
+
+    private LocalDateTime lockedUntil;
+
+    @CreatedDate // SE LLENA AUTOMATICAMENTE AL CREAR
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // SE ACTUALIZA AUTOMATICAMENTE
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime lastLoginAt;
+
+    private LocalDateTime deletedAt;
+
+}
