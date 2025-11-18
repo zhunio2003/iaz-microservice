09008d7e5e684841dc9f1df4b556ae2d681f4f4d
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2025-11-17 23:10:31 -0500

    feat(AuthServie): Configuration UserRepository

--- auth-service/src/main/java/com/mazr/iaz/microservices/entity/User.java
+++ auth-service/src/main/java/com/mazr/iaz/microservices/entity/User.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- auth-service/src/main/java/com/mazr/iaz/microservices/repository/UserRepository.java
+++ auth-service/src/main/java/com/mazr/iaz/microservices/repository/UserRepository.java
@@ -10,12 +10,18 @@ import com.mazr.iaz.microservices.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 
-    Boolean existsByUsername(String username);
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
+    Optional<User> findByUsernameAndDeletedAtIsNull(String username);
 
-    Boolean existsByEmail(String email);
+    Boolean existsByEmailAndDeletedAtIsNull(String email);
+    Boolean existsByUsernameAndDeletedAtIsNull(String username);
+
+
     
 }
